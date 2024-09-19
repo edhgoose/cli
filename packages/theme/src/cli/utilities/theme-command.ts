3a8f913b0c8d25d3d901d26d785ba3e83b78511b
James Meng <james.meng@shopify.com>
2024-09-19 14:30:52 -0700

    Re-arrange + export ThemeCommand helpers

--- packages/theme/src/cli/utilities/theme-command.ts
+++ packages/theme/src/cli/utilities/theme-command.ts
@@ -4,29 +4,33 @@ import Command from '@shopify/cli-kit/node/base-command'
 export interface FlagValues {
   [key: string]: boolean | string | string[] | number | undefined
 }
-interface PassThroughFlagsOptions {
+export interface PassThroughFlagsOptions {
   // Only pass on flags that are relevant to CLI2
   allowedFlags?: string[]
 }
 
 export default abstract class ThemeCommand extends Command {
   passThroughFlags(flags: FlagValues, {allowedFlags}: PassThroughFlagsOptions): string[] {
-    const passThroughFlags: string[] = []
-    for (const [label, value] of Object.entries(flags)) {
-      if (!(allowedFlags ?? []).includes(label)) {
-        continue
-      } else if (typeof value === 'boolean') {
-        if (value) passThroughFlags.push(`--${label}`)
-      } else if (Array.isArray(value)) {
-        value.forEach((element) => passThroughFlags.push(`--${label}`, `${element}`))
-      } else {
-        passThroughFlags.push(`--${label}`, `${value}`)
-      }
-    }
-    return passThroughFlags
+    return passThroughFlags(flags, {allowedFlags})
   }
 
   environmentsFilename(): string {
     return configurationFileName
   }
+}
+
+export function passThroughFlags(flags: FlagValues, {allowedFlags}: PassThroughFlagsOptions): string[] {
+  const passThroughFlags: string[] = []
+  for (const [label, value] of Object.entries(flags)) {
+    if (!(allowedFlags ?? []).includes(label)) {
+      continue
+    } else if (typeof value === 'boolean') {
+      if (value) passThroughFlags.push(`--${label}`)
+    } else if (Array.isArray(value)) {
+      value.forEach((element) => passThroughFlags.push(`--${label}`, `${element}`))
+    } else {
+      passThroughFlags.push(`--${label}`, `${value}`)
+    }
+  }
+  return passThroughFlags
 }
