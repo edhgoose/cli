74584ef56533bccf3f607083554e209f740d7f8a
James Meng <james.meng@shopify.com>
2024-09-19 15:34:53 -0700

    Update tests
    Move + fix CLI2 tests
    
    Remove pull command test
    
    Remove CLI2 Invocation + Tests

--- packages/theme/src/cli/services/pull.ts
+++ packages/theme/src/cli/services/pull.ts
@@ -6,15 +6,12 @@ import {showEmbeddedCLIWarning} from '../utilities/embedded-cli-warning.js'
 import {ensureThemeStore} from '../utilities/theme-store.js'
 import {DevelopmentThemeManager} from '../utilities/development-theme-manager.js'
 import {findOrSelectTheme} from '../utilities/theme-selector.js'
-import {FlagValues, passThroughFlags} from '../utilities/theme-command.js'
-import Pull from '../commands/theme/pull.js'
 import {Theme} from '@shopify/cli-kit/node/themes/types'
 import {AdminSession, ensureAuthenticatedThemes} from '@shopify/cli-kit/node/session'
 import {fetchChecksums} from '@shopify/cli-kit/node/themes/api'
 import {renderSuccess} from '@shopify/cli-kit/node/ui'
 import {glob} from '@shopify/cli-kit/node/fs'
-import {useEmbeddedThemeCLI} from '@shopify/cli-kit/node/context/local'
-import {execCLI2} from '@shopify/cli-kit/node/ruby'
+import {cwd} from '@shopify/cli-kit/node/path'
 
 interface PullOptions {
   path: string
@@ -25,18 +22,18 @@ interface PullOptions {
 }
 
 export interface PullFlags {
-  path: string
+  path?: string
   password?: string
   environment?: string
   store?: string
   theme?: string
-  development: boolean
-  live: boolean
-  nodelete: boolean
+  development?: boolean
+  live?: boolean
+  nodelete?: boolean
   only?: string[]
   ignore?: string[]
-  force: boolean
-  legacy: boolean
+  force?: boolean
+  legacy?: boolean
 }
 
 export async function pull(flags: PullFlags) {
@@ -48,37 +45,23 @@ export async function pull(flags: PullFlags) {
   const developmentThemeManager = new DevelopmentThemeManager(adminSession)
   const developmentTheme = await (flags.development ? developmentThemeManager.find() : developmentThemeManager.fetch())
 
-  if (!flags.legacy) {
-    const {path, nodelete, live, development, only, ignore, force} = flags
+  const {path, nodelete, live, development, only, ignore, force} = flags
 
-    const theme = await findOrSelectTheme(adminSession, {
-      header: 'Select a theme to open',
-      filter: {
-        live,
-        theme: development ? `${developmentTheme?.id}` : flags.theme,
-      },
-    })
+  const theme = await findOrSelectTheme(adminSession, {
+    header: 'Select a theme to open',
+    filter: {
+      live,
+      theme: development ? `${developmentTheme?.id}` : flags.theme,
+    },
+  })
 
-    await executePull(theme, adminSession, {path, nodelete, only, ignore, force})
-    return
-  }
-
-  const flagsForCli2 = flags as typeof flags & FlagValues
-
-  if (developmentTheme) {
-    if (flagsForCli2.development) {
-      flagsForCli2.theme = `${developmentTheme.id}`
-      flagsForCli2.development = false
-    }
-    if (useEmbeddedThemeCLI()) {
-      flagsForCli2['development-theme-id'] = developmentTheme.id
-    }
-  }
-
-  const flagsToPass = passThroughFlags(flagsForCli2, {allowedFlags: Pull.cli2Flags})
-  const command = ['theme', 'pull', flagsForCli2.path, ...flagsToPass]
-
-  await execCLI2(command, {store, adminToken: adminSession.token})
+  await executePull(theme, adminSession, {
+    path: path || cwd(),
+    nodelete: nodelete || false,
+    only: only || [],
+    ignore: ignore || [],
+    force: force || false,
+  })
 }
 
 async function executePull(theme: Theme, session: AdminSession, options: PullOptions) {
